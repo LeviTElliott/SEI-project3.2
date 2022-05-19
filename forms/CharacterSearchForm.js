7b3ef0c9620565a17402a045dbdc967089172a7a
Levi Elliott <levihackworth@yahoo.com>
2022-05-19 08:06:17 -0500

    thurs start

---
+++ forms/CharacterSearchForm.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import {getToken, getCharacterProfile, realmList } from '../api/Battlenet';
+
+const CharacterSearchForm = props => {
+    const profileData = {region: "us", realm: "", name: ""} 
+    const [profile, setProfile] = useState(profileData);
+    
+
+    const handleChange = e => {
+      const{name, value} = e.target
+      setProfile({...profile, [name]: value});
+    }
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      getChar(profile).then(response => {
+        if(response["name"].toLowerCase() === profile.name.toLowerCase()){
+          props.setCharacter(response)
+          setProfile(profileData);
+        }else{
+          console.log("Character dose not exist")
+          props.setCharacter({error: "Error"})
+        }
+      });
+    }
+
+    async function getChar(params) {
+      let t = await getToken();
+      let c = await getCharacterProfile(t, params.region, params.realm, params.name);
+      console.log(c)
+      return c 
+    }
+    
+    const selectList = realmList.map(r =>
+        <option key={r} value={r}>
+          {r}
+        </option>
+    );
+
+    return(
+       <form onSubmit={handleSubmit} class="char-form">
+       <div class="flex-row">
+       <div class="flex-small">
+         <select name="realm" value={profile.realm} onChange={handleChange}>
+         <option >Choose realm</option>
+           {selectList}
+         </select>
+        </div>
+        <div class="flex-small">
+         <input type="text" name="name" value={profile.name} onChange={handleChange} />
+        </div>
+        </div>
+       </form>
+    );
+  }
+  
+  export default CharacterSearchForm;
---
+++ pages/CharacterProfilePage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {classList} from '../api/Battlenet';
+
+const CharacterProfilePage = props => {
+
+    let characterProp = Object.keys(props.character).map(key => {
+      if (key !== "appearance"){ 
+        return <li key={key} >{key}: {props.character[key]} </li>
+      }
+    })
+
+    const characterProfile = (
+      
+      <div class="flex-row">         
+        <div class="flex-small">
+          <img src={"https://render-us.worldofwarcraft.com/character/" + props.character["thumbnail"]} alt="profile"/>
+          <h1>{ props.character["name"] }</h1> 
+          <div class="role">{ props.character["realm"] }, { classList[props.character["class"]] }</div>
+          <div class="level">{ props.character["level"] }</div>    
+        </div>
+      </div>
+      
+    )
+
+    return(
+      <div class="flex-small">
+
+          { props.character.hasOwnProperty('error') ?(
+            <div class="vertical-center content-section">Cant find that character</div>
+          ):(
+            <div class="container character">
+            {characterProfile}
+            </div>
+          )}
+          
+        
+      </div>
+    );
+  
+  }
+
+  export default CharacterProfilePage;
